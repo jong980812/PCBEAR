1c3bd1128879a297bc4fddc2415f3a25266e732a
jong980812 <jong980812@gmail.com>
2025-03-31 17:17:14 +0900

    pose estimation

--- Pose_extraction/inference.sh
+++ Pose_extraction/inference.sh
@@ -5,18 +5,18 @@
 #SBATCH --mem-per-gpu=50G
 #SBATCH --time 4-00:00:0
 #SBATCH --partition batch
-#SBATCH -w augi3
+#SBATCH -w vll2
 #SBATCH -o penn_action_test.out
 #SBATCH -e penn_action_test.err
 
 export OPENCV_FFMPEG_CAPTURE_OPTIONS="loglevel;quiet"
 python /data/jongseo/project/PCBEAR/Pose_extraction/inference_folder.py \
     --output-path "./" \
-    --model "/data/jongseo/project/PCBEAR/Pose_extraction/vitpose-b-coco.pth" \
+    --model "/data/jongseo/project/PCBEAR/Pose_extraction/vitpose-l-coco.pth" \
     --yolo "/data/jongseo/project/PCBEAR/Pose_extraction/yolov8l.pt" \
     --dataset "coco" \
     --det-class "human" \
-    --model-name "b" \
+    --model-name "l" \
     --yolo-size "320" \
     --conf-threshold "0.3" \
     --rotate "0" \
@@ -24,5 +24,6 @@ python /data/jongseo/project/PCBEAR/Pose_extraction/inference_folder.py \
     --save-img \
     --save-json \
     --single-pose \
+
 
 
