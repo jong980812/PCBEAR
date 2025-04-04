fea420383a97a919ef28fec6bc261e2da4c674ff
jong980812 <jong980812@gmail.com>
2025-04-04 18:08:06 +0900

    pose estimation arg 추가

--- Pose_extraction/inference.sh
+++ Pose_extraction/inference.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name Penn_extract_skeleton
+#SBATCH --job-name kth_extract_skeleton
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu=12
 #SBATCH --mem-per-gpu=50G
 #SBATCH --time 4-00:00:0
 #SBATCH --partition batch
-#SBATCH -w vll2
-#SBATCH -o penn_action_test.out
-#SBATCH -e penn_action_test.err
+#SBATCH -w vll3
+#SBATCH -o biggest_box_video.out
+#SBATCH -e biggest_box_video.err
 
 export OPENCV_FFMPEG_CAPTURE_OPTIONS="loglevel;quiet"
 python /data/jongseo/project/PCBEAR/Pose_extraction/inference_folder.py \
     --output-path "./" \
+    --src_dir /local_datasets/haa500/video \
+    --skeleton_video_dir /local_datasets/haa500/video/biggest_box_video \
+    --skeleton_json_dir /local_datasets/haa500/video/biggest_box_json \
     --model "/data/jongseo/project/PCBEAR/Pose_extraction/vitpose-l-coco.pth" \
     --yolo "/data/jongseo/project/PCBEAR/Pose_extraction/yolov8l.pt" \
     --dataset "coco" \
@@ -24,6 +27,8 @@ python /data/jongseo/project/PCBEAR/Pose_extraction/inference_folder.py \
     --save-img \
     --save-json \
     --single-pose \
+    --class_folders
+
 
 
 
